0d30ff2acfde95405208a9a809ea37f447822922
Matt Darby <matt.darby@edgecase.com>
2011-10-18 15:54:20 -0400

    Made meeting date calculator more robust

--- server.rb
+++ server.rb
@@ -5,22 +5,36 @@ require 'sass'
 require 'yaml'
 require 'active_support/all'
 
+class MeetingDates
+  def initialize(relative_date = Date.today)
+    @crb_dates = []
+    @jam_dates = []
+    calc_meeting_dates
+  end
+
+  def calc_meeting_dates
+    start_date = Date.today.beginning_of_year
+    (0..12).inject([]){|s, num| dates_for_month(start_date + num.months)}
+  end
+
+  def dates_for_month(relative_date)
+    s = relative_date.beginning_of_month
+    e = relative_date.end_of_month
+    crb_date = (s..e).select{|d| d.wday == 1}[2] + 18.5.hours
+    @crb_dates << crb_date
+    @jam_dates << crb_date + 9.days
+  end
+
+  def next_crb_meeting
+    @crb_dates.find{|d| d > Time.now}
+  end
+
+  def next_jam_meeting
+    @jam_dates.find{|d| d > Time.now}
+  end
+end
+
 helpers do
-  def next_crb_meeting_date
-    @next_crb_meeting_date ||= begin
-      s = Date.today.beginning_of_month
-      e = Date.today.end_of_month
-      date = (s..e).select{|d| d.wday == 1}[2]
-      date + 18.5.hours
-    end
-  end
-
-  def next_code_jam_date
-    @next_code_jam_date ||= begin
-      next_crb_meeting_date + 9.days
-    end
-  end
-
   def formatted_date(date)
     day = date.day.ordinalize
     date.strftime("%A, %B #{day}, %Y at %l:%M%P")
@@ -33,11 +47,12 @@ configure do
 end
 
 get '/' do
+  dates     = MeetingDates.new
   info      = YAML::load(File.open("meetings.yml"))
-  @next_crb = next_crb_meeting_date
-  @next_jam = next_code_jam_date
+  @next_crb = dates.next_crb_meeting
+  @next_jam = dates.next_jam_meeting
   date      = @next_crb.strftime('%D')
-  @speakers = info[date]
+  @speakers = info[date] || []
 
   haml :index
 end

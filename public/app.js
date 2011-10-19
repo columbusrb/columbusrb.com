b318b8eb2bc051bfa4f06769c0c4f4762546f355
Matt Darby <matt.darby@edgecase.com>
2011-10-19 12:36:54 -0400

    Tightened up tweets

--- public/app.js
+++ public/app.js
@@ -6,18 +6,21 @@ function autoLinkTweet(text){
 };
 
 function loadTweets(){
-  var div = $('#tweets');
-  var url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=columbusrb&count=5&trim_user=1&exclude_replies=1";
+  var target    = $('#tweets');
+  var template  = $('#tweetTemplate').html();
+  var tweet_url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=columbusrb&count=5&trim_user=1&exclude_replies=1";
 
   $.ajax({
-    url: url,
+    url: tweet_url,
     dataType: 'jsonp',
     success: function(data){
       for(var i=0; i<5; i++){
         var tweet = data[i];
         var text  = autoLinkTweet(tweet.text);
-        var li    = $('<li>').html(text);
-        div.append(li);
+        var time  = Date.create(tweet.created_at).relative();
+        var html  = _.template(template, {tweet: text, time: time});
+        var li    = $('<li>').html(html);
+        target.append(li);
       }
     }
   });

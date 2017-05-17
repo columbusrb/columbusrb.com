034c142a5add691e916f653c82b961189a751515
Rachel Slaby <rslaby@covermymeds.com>
2017-05-17 19:20:55 -0400

    Removed old stuff, added feedback survey link

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Columbusrb::Application.routes.draw do
   post '/speakers/add', to: "pages#add_speaker"
 
   root to: "pages#now"
+
+  get 'feedback' => redirect("https://www.surveymonkey.com/r/JB9JX8X")
 end

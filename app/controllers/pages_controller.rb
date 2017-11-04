421a5b1ddbdeab6a905f11b8666b7caed0ea060e
Glenn Harmon <gharmon@covermymeds.com>
2017-11-04 19:28:17 -0400

    removed 72 insert

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
   end
 
   def now
-    @next_crb = Meeting.next_crb
+    @next_crb = Meeting.find_or_create_next_date
     @board_members = BoardMember.all.order(director: :desc, title: :asc)
   end
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,26 +4,35 @@ class Meeting < ActiveRecord::Base
 
   attr_accessor :crb_times
 
-  scope :next_crb, -> { where(["DATE(time) >= ?", Date.today]).order('time ASC').limit(1).first }
+  scope :next_crb, -> { where(["DATE(time) >= ?", Date.today]).order('time ASC').limit(1) }
   scope :lecture, -> { where(["format = ?", "Lecture"]) }
+  scope :current_month_crb, -> { where(["DATE(time) >? AND DATE(time)<?", Time.now.beginning_of_month, Time.now.end_of_month]).order('time ASC').limit(1) }
 
   def self.add_speaker_to_next_meeting(name, title, url)
     next_crb.speakers.create!({name: name, title:title, url: url})
   end
 
+  def self.find_or_create_next_date
+    if Meeting.next_crb.blank?
+      if Meeting.current_month_crb.blank?
+        next_month = 0
+      else
+        next_month = 1
+      end
 
-  def self.insert_times_for_five_years
-    beginning = Date.today.beginning_of_month - 2.month
+      beginning = Date.today.beginning_of_month
 
-    72.times do |x|
-      curr_time = beginning + (x+1).months
+      curr_time = beginning + next_month.months
       s         = curr_time.beginning_of_month
       e         = curr_time.end_of_month
       crb_time  = (s..e).select{|d| d.wday == 1}[2] + 18.5.hours
 
       Meeting.find_or_create_by(time: crb_time)
+    else
+      next_crb
     end
   end
+
 
   def to_s
     time

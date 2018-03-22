48f2c18c6a9f0c5ee261f690229b47c4202dd199
Rachel Slaby <rslaby@covermymeds.com>
2018-03-21 20:46:59 -0400

    Removed duplicate and unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Columbusrb::Application.routes.draw do
 
   resources :speakers
-  resources :speakers
-  post '/speakers/add', to: "pages#add_speaker"
 
   root to: "pages#now"
 
   get 'feedback' => redirect("https://www.surveymonkey.com/r/JB9JX8X")
 
-  get '/companies', to: "pages#companies"
 end

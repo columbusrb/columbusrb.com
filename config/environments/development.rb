5b5f15d67efb73493d36ff4299642f831dc52155
Aberham Haile <aberhamhaile@Aberhams-MacBook-Pro.local>
2019-08-10 11:48:52 -0400

    added meetup.com api get call to show up to date members count for crb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #meetup api server url
+  config.meetup_api_server_url = "https://api.meetup.com/"
 end

a9d68a0de985f014b55aca54f1874fafe9c6dca6
Jon Canady <jon@joncanady.com>
2020-07-01 14:30:36 -0400

    Convert `require "rails/all"` to an individual list of `require`s.
    This is the list you'd get if we'd generated the app with --skip-sprockets. It's
    ridiculous that this is the best method to get rid of the asset pipeline but
    there you are.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,25 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+
+# How I got this list:
+# generated a new Rails app with --skip-sprockets
+# copied their application.rb  section
+# this is stupid
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +28,7 @@ Bundler.require(*Rails.groups)
 module Columbusrb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

a2a78739ee26d7ff751ed25027dc50ede0b98d1b
Jon Canady <jon@joncanady.com>
2020-07-01 14:35:51 -0400

    Continue to get rid of asset pipeline.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,18 +12,6 @@ Columbusrb::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_files = true
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
-  # Generate digests for assets URLs
-  config.assets.digest = true
-
-  # Defaults to Rails.root.join("public/assets")
-  # config.assets.manifest = YOUR_PATH
-
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
@@ -44,10 +32,7 @@ Columbusrb::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  # config.action_controller.asset_host = "http://dorp.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -65,9 +50,7 @@ Columbusrb::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  config.assets.precompile += %w(active_admin.css active_admin.js active_admin/print.css)
-
   #meetup api server url
+
   config.meetup_api_server_url = "https://api.meetup.com/"
 end

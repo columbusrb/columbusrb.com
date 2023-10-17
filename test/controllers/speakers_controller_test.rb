72360541d38a66f8d66dc23ca30ed7a64ee6d9db
Jared Szechy <jared.szechy@gmail.com>
2023-10-16 23:56:37 -0400

    Fix tests

--- test/controllers/speakers_controller_test.rb
+++ test/controllers/speakers_controller_test.rb
@@ -1,47 +1,59 @@
 require 'test_helper'
 
-class SpeakersControllerTest < ActionController::TestCase
+class SpeakersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @speaker = speakers(:one)
+    @meeting = Meeting.find_or_create_next_date
+    sign_in admin_users(:admin)
   end
 
   test "should get index" do
-    get :index
+    get admin_speakers_url
+
     assert_response :success
-    assert_not_nil assigns(:speakers)
   end
 
   test "should get new" do
-    get :new
+    get new_admin_speaker_url
+
     assert_response :success
   end
 
   test "should create speaker" do
     assert_difference('Speaker.count') do
-      post :create, speaker: { name: @speaker.name, title: @speaker.title, url: @speaker.url }
+      post admin_speakers_url, params: {
+        speaker: { name: @speaker.name, title: @speaker.title, url: @speaker.url, meeting_id: @meeting.id }
+      }
     end
 
-    assert_redirected_to admin_speaker_path(assigns(:speaker))
+    assert_redirected_to admin_speaker_path(Speaker.last)
   end
 
   test "should show speaker" do
-    get :show, id: @speaker
+    get admin_speaker_url(@speaker)
+
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @speaker
+    get edit_admin_speaker_url(@speaker)
+
     assert_response :success
   end
 
   test "should update speaker" do
-    patch :update, id: @speaker, speaker: { name: @speaker.name, title: @speaker.title, url: @speaker.url }
-    assert_redirected_to admin_speaker_path(assigns(:speaker))
+    patch admin_speaker_url(@speaker), params: {
+      speaker: { name: @speaker.name, title: @speaker.title, url: @speaker.url, meeting_id: @meeting.id }
+    }
+
+    assert_redirected_to admin_speaker_path(@speaker)
   end
 
   test "should destroy speaker" do
     assert_difference('Speaker.count', -1) do
-      delete :destroy, id: @speaker
+      delete admin_speaker_url(@speaker)
     end
 
     assert_redirected_to admin_speakers_path
